f6cc5ca2655594507adc62f9dc6ce6210fb9ec99
Xhamolk <amilcar.menjivar11@gmail.com>
2013-01-20 21:49:05 -0600

    Added the documentation for IInventoryHandler

--- common/net/minecraftforge/inventory/IInventoryHandler.java
+++ common/net/minecraftforge/inventory/IInventoryHandler.java
@@ -7,8 +7,22 @@ import net.minecraftforge.common.ForgeDirection;
 
 import java.util.ArrayList;
 
+/**
+ * The handler interface for any inventory.
+ * The implementations of this interface are what do the managing/manipulating inventories.
+ *
+ * The ICustomInventory interface is provided to
+ * @see ICustomInventory
+ */
 public interface IInventoryHandler {
 
+	/**
+	 * List all the ingredients that can be found on this inventory.
+	 *
+	 * @param inventory the inventory that holds the items.
+	 * @param side the side from which the inventory is accessed (only applies to ISidedInventory)
+	 * @return the list that holds all the ingredients stored on this inventory.
+	 */
 	public ArrayList<ItemStack> listItemsInInventory(IInventory inventory, ForgeDirection side);
 
 	/**
@@ -76,28 +90,82 @@ public interface IInventoryHandler {
 	 */
 	public ItemStack takeItemFromInventory(IInventory inventory, ForgeDirection side);
 
+	/**
+	 * Takes from the inventory the first stack of the item passed.
+	 *
+	 * @param inventory the inventory from where to take the item.
+	 * @param item the item to be taken. Only non-null values allowed.
+	 * @param side the side from which the inventory is accessed.
+	 *             This will be ignored unless the inventory is ISidedInventory.
+	 * @return the ItemStack taken.
+	 */
 	public ItemStack takeItemFromInventory(IInventory inventory, ItemStack item, ForgeDirection side);
 
+	/**
+	 * Takes from the inventory the amount specified of the item passed.
+	 *
+	 * If there is not enough to fulfill the request, will return as much as possible.
+	 * Keep in mind that the max amount returned is limited by the item's max stack size.
+	 * @param inventory the inventory from where to take the item.
+	 * @param item the item to be taken. Only non-null values allowed.
+     * @param quantity the amount to take of this item.
+	 * @param side the side from which the inventory is accessed.
+	 *             This will be ignored unless the inventory is ISidedInventory.
+	 * @return the ItemStack taken.
+	 */
 	public ItemStack takeItemFromInventory(IInventory inventory, ItemStack item, int quantity, ForgeDirection side);
 
+	/**
+	 * Takes the entire stack from the inventory slot.
+	 *
+	 * @param inventory the inventory from where to take the item.
+	 * @param slotIndex the inventory slot from where to take the item.
+	 * @return the ItemStack taken, or null if the inventory slot is empty.
+	 */
 	public ItemStack takeItemFromInventorySlot(IInventory inventory, int slotIndex);
 
-	// will manipulate the inventoryStack's stack size
-	// might return less than requested, unless there is plenty to fulfill the request.
-	// prefer: takeItemFromInventory instead
+	/**
+	 * Takes the specified amount of the item on the inventory slot.
+	 *
+	 * Prefer using takeItemFromInventory instead.
+	 *
+	 * @param inventory the inventory from where to take the item.
+	 * @param slotIndex the inventory slot from where to take the item.
+	 * @param quantity the amount to take
+	 * @return the ItemStack taken, or null if the slot is empty.
+	 */
 	public ItemStack takeItemFromInventorySlot(IInventory inventory, int slotIndex, int quantity);
 
+	/**
+	 * Get the amount available of the specified item in the inventory.
+	 *
+	 * @param inventory the inventory to check for the item.
+	 * @param itemStack the item to match on the inventory.
+	 * @param side the side from which the inventory is accessed.
+	 *             This will be ignored unless the inventory is ISidedInventory.
+	 * @return the amount available of the specified item in the inventory.
+	 */
 	public int getItemCountInInventory(IInventory inventory, ItemStack itemStack, ForgeDirection side);
 
 	/**
-	 * Get the amount of items available on the inventory slot.
+	 * Get the amount available of items in the inventory slot.
 	 *
 	 * @param inventory the inventory to check for items.
 	 * @param slotIndex the inventory slot to check.
-	 * @return the amount available of any item in the inventory slot. Or 0, if no item is in the slot.
+	 * @return the amount available of any item in the inventory slot.
+	 *      Or 0, if the slot is empty.
 	 */
 	public int getItemCountInSlot(IInventory inventory, int slotIndex);
 
+	/**
+	 * Get the amount available of the specified item on the inventory slot.
+	 *
+	 * @param inventory the inventory checked.
+	 * @param slotIndex the inventory slot to check.
+	 * @param itemStack the item to match on the inventory slot.
+	 * @return the amount available of the item specified.
+	 *      Or 0, if the slot is empty or doesn't match with <code>itemStack</code>.
+	 */
 	public int getItemCountInSlot(IInventory inventory, int slotIndex, ItemStack itemStack);
 
 }
